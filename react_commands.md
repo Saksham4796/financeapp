9950846dbb708159c7279b20aab5d47ed1549762
Saksham4796 <sakshamsahai4796@gmail.com>
2023-03-17 05:26:02 -0600

    intial commit

---
+++ react_commands.md
@@ -0,0 +1,9 @@
+#### Command Line to check if node is installed
+node -version
+
+#### Command Line to create an application using React
+npx create-react-app .
+The above command creates the applciation in the current folder
+
+#### Command Line to start the development server
+npm start
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      income: 0,
+      expenses: 0,
+      savings: 0,
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { income, expenses } = this.state;
+    const savings = income - expenses;
+
+    this.setState({
+      savings: savings,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>My Finances</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Income:
+            <input
+              name="income"
+              type="number"
+              value={this.state.income}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Expenses:
+            <input
+              name="expenses"
+              type="number"
+              value={this.state.expenses}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Calculate Savings</button>
+        </form>
+        <h2>Monthly Savings: {this.state.savings}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
